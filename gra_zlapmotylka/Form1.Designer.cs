157cc9404a3524a77da55a5cb25de90581dea764
takiesobiedrzewo <97553812+takiesobiedrzewo@users.noreply.github.com>
2022-11-18 19:29:57 +0100

    Dodaj pliki projektów.

---
+++ gra_zlapmotylka/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace gra_zlapmotylka
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblLife = new System.Windows.Forms.TextBox();
+            this.lblPoints = new System.Windows.Forms.TextBox();
+            this.butterfly = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.butterfly)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblLife
+            // 
+            this.lblLife.BackColor = System.Drawing.Color.White;
+            this.lblLife.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblLife.Location = new System.Drawing.Point(581, 21);
+            this.lblLife.Name = "lblLife";
+            this.lblLife.Size = new System.Drawing.Size(144, 32);
+            this.lblLife.TabIndex = 0;
+            this.lblLife.Text = "Zycia: ";
+            // 
+            // lblPoints
+            // 
+            this.lblPoints.BackColor = System.Drawing.Color.White;
+            this.lblPoints.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblPoints.Location = new System.Drawing.Point(581, 59);
+            this.lblPoints.Name = "lblPoints";
+            this.lblPoints.Size = new System.Drawing.Size(144, 32);
+            this.lblPoints.TabIndex = 1;
+            this.lblPoints.Text = "Punkty: ";
+            // 
+            // butterfly
+            // 
+            this.butterfly.BackColor = System.Drawing.Color.Transparent;
+            this.butterfly.BackgroundImage = global::gra_zlapmotylka.Properties.Resources.blue;
+            this.butterfly.Location = new System.Drawing.Point(177, 149);
+            this.butterfly.Name = "butterfly";
+            this.butterfly.Size = new System.Drawing.Size(160, 106);
+            this.butterfly.TabIndex = 2;
+            this.butterfly.TabStop = false;
+            this.butterfly.Click += new System.EventHandler(this.gotButterfly);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.moveButterfly);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::gra_zlapmotylka.Properties.Resources.background;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.butterfly);
+            this.Controls.Add(this.lblPoints);
+            this.Controls.Add(this.lblLife);
+            this.Name = "Form1";
+            this.Text = "Zlap Motylka";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            ((System.ComponentModel.ISupportInitialize)(this.butterfly)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox lblLife;
+        private System.Windows.Forms.TextBox lblPoints;
+        private System.Windows.Forms.PictureBox butterfly;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ gra_zlapmotylka/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gra_zlapmotylka
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        int locationNum = 0;
+        int points = 0;
+        int lifes = 3;
+        bool isClicked = false;
+        bool isBlue = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void gotButterfly(object sender, EventArgs e)
+        {
+            if (isBlue == true)
+            {
+                points++;
+
+                isClicked = true;
+
+                butterfly.Enabled = false;
+            }
+        }
+
+        private void moveButterfly(object sender, EventArgs e)
+        {
+            lblLife.Text = "Zycia " + lifes;
+            lblPoints.Text = "Punkty:" + points;
+            if ((isClicked == false && isBlue == true) || (isClicked == true && isBlue == false))
+            {
+                lifes--;
+            }
+            if (points > 15)
+            {
+                timer1.Stop();
+                butterfly.Enabled = false;
+                MessageBox.Show("You Win");
+            }
+            else if (lifes < 0)
+            {
+                timer1.Stop();
+                butterfly.Enabled = false;
+                MessageBox.Show("You Lose");
+
+            }
+
+            moveButterfly();
+        }
+        private void moveButterfly()
+        {   
+            isClicked = false;
+            butterfly.Enabled = true;
+            locationNum = rnd.Next(2);
+            if (locationNum == 1)
+            {
+                butterfly.Image = Properties.Resources.red;
+                isBlue = false;
+            }
+            else
+            {
+                butterfly.Image = Properties.Resources.blue;
+                isBlue = true;
+            }
+            butterfly.BackColor = System.Drawing.Color.Transparent;
+
+            locationNum = rnd.Next(1, 7);
+
+
+            switch (locationNum)
+            {
+                case 1:
+                    butterfly.Left = 434;
+                    butterfly.Top = 249;
+                    break;
+
+                case 2:
+                    butterfly.Left = 257;
+                    butterfly.Top = 211;
+                    break;
+
+                case 3:
+                    butterfly.Left = 58;
+                    butterfly.Top = 240;
+                    break;
+
+                case 4:
+                    butterfly.Left = 85;
+                    butterfly.Top = 318;
+                    break;
+
+                case 5:
+                    butterfly.Left = 254;
+                    butterfly.Top = 364;
+                    break;
+
+                case 6:
+                    butterfly.Left = 416;
+                    butterfly.Top = 323;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+
+    }
+}
